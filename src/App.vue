3c6ee545dcf8cbee64ceb0a230f4baed41d13abc
halouma <essidahlem.info1997@gmail.com>
2022-06-03 12:51:52 +0200

    weather app

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         <input
           type="text"
           class="search-bar"
-          placeholder="Search..."
+          placeholder="Rechercher un lieu..."
           v-model="query"
           @keypress="fetchWeather"
         />
